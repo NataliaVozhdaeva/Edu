844095ca6b20d5791a27b959c62c537a1a897e2c
NataliaVozhdaeva <LomzNat@yandex.ru>
2022-09-06 23:40:05 +0200

    citySelect css done +js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,23 +1,62 @@
-let menuArrowNext = document.querySelector('.header_btn__next');
-let menuArrowPrev = document.querySelector('.header_btn__prev');
-let headerNav = document.querySelector('.header_navigation');
-let navLastChild = document.querySelector('.navigation_item:last-child');
-let navFirstChild = document.querySelector('.navigation_item:first-child');
-let nav2ndChild = document.querySelector('.navigation_item:nth-child(2)');
+/* прокрутка меню в шапке */
+
+const menuArrowNext = document.querySelector('.header_btn__next');
+const menuArrowPrev = document.querySelector('.header_btn__prev');
+const headerNav = document.querySelector('.header_navigation');
+const navLastChild = document.querySelector('.navigation_item:last-child');
+const navFirstChild = document.querySelector('.navigation_item:first-child');
+const nav2ndChild = document.querySelector('.navigation_item:nth-child(2)');
 /* Можно было бы применить эффект угасания текста только к первому и последнему элементам как в макете 
 и укоротить код, но при небольшой ширине первого красивее накладывать эффект на второй */
 
-let toggleClass = (el1, el2, className) => {
+const toggleMenuItemsClass = (el1, el2, className) => {
   el1.classList.toggle(className);
   el2.classList.toggle(className);
 };
 
-let changeMenuPosition = () => {
+const changeMenuPosition = () => {
   headerNav.classList.toggle('translateToLeft');
   navFirstChild.classList.toggle('hidden');
-  toggleClass(navLastChild, nav2ndChild, 'active');
-  toggleClass(menuArrowNext, menuArrowPrev, 'notDisplay');
+  toggleMenuItemsClass(navLastChild, nav2ndChild, 'active');
+  toggleMenuItemsClass(menuArrowNext, menuArrowPrev, 'notDisplay');
 };
 
 menuArrowNext.addEventListener('click', changeMenuPosition);
 menuArrowPrev.addEventListener('click', changeMenuPosition);
+
+/* Код для вкладки выбора регионов */
+
+const cities = document.querySelectorAll('.city_item');
+
+/* const addToFiltered = (el) => {
+  const filteredCitiesContainer = document.querySelector(
+    '.citySearchMenu_doneContainer'
+  );
+  const filteredCity = document.createElement('span');
+  filteredCity.classList.add('citySearchMenu_done');
+  filteredCity.textContent = el.textcontent;
+  filteredCitiesContainer.appendChild(filteredCity);
+}; */
+
+for (let i = 0; i < cities.length; i++) {
+  cities[i].addEventListener('click', function () {
+    const filteredCitiesContainer = document.querySelector(
+      '.citySearchMenu_doneContainer'
+    );
+    const filteredCity = document.createElement('span');
+    filteredCity.classList.add('citySearchMenu_done');
+    filteredCity.textContent = cities[i].textcontent;
+    filteredCitiesContainer.appendChild(filteredCity);
+  });
+}
+
+//console.log(cities);
+
+const filteredCities = document.querySelectorAll('.citySearchMenu_done');
+
+for (let i = 0; i < filteredCities.length; i++) {
+  const delCityBtn = filteredCities[i].querySelector('.citySearchMenu_delBtn');
+  delCityBtn.addEventListener('click', function () {
+    filteredCities[i].remove();
+  });
+}
